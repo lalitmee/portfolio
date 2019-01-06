1fc4794e927db6a8f1146053d338e95df48465e6
lalitmee <lalitkumar.meena.lk@gmail.com>
2019-01-06 22:44:11 +0530

    Big Change
    Semantic Ui uninstalled and Mateial Ui installed
    Navbar component is created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from 'react';
-import {
-  Button,
-  Container,
-  Header,
-  Image,
-  Menu,
-  Segment
-} from 'semantic-ui-react';
+import './App.css';
+import NavBar from './components/NavBar/NavBar';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -16,63 +10,10 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <Container>
-          <Segment inverted textAlign="center" vertical>
-            <Menu size="large" inverted>
-              <Menu.Item as="a">
-                <Image
-                  size="mini"
-                  src="https://avatars1.githubusercontent.com/u/10762218?s=460&v=4"
-                  circular
-                />{' '}
-                {/* Lalit Kumar */}{' '}
-              </Menu.Item>{' '}
-              <Menu.Item as="a"> Home </Menu.Item>{' '}
-              <Menu.Item as="a"> Work </Menu.Item>{' '}
-              <Menu.Item as="a"> Company </Menu.Item>{' '}
-              <Menu.Item as="a"> Careers </Menu.Item>{' '}
-              <Menu.Item position="right">
-                <Button as="a"> Log in </Button>{' '}
-                <Button
-                  as="a"
-                  style={{
-                    marginLeft: '0.5em'
-                  }}
-                >
-                  Sign Up{' '}
-                </Button>{' '}
-              </Menu.Item>{' '}
-            </Menu>{' '}
-          </Segment>{' '}
-          <Segment
-            className="heading"
-            style={{
-              minHeight: '300px',
-              textAlign: 'center',
-              margin: 0,
-              padding: 0
-            }}
-          >
-            <Header
-              as="h1"
-              style={{
-                marginTop: '3em',
-                fontSize: '3em'
-              }}
-            >
-              Lalit Kumar{' '}
-            </Header>{' '}
-            <Header
-              as="h4"
-              style={{
-                marginTop: '-15px'
-              }}
-            >
-              Software Developer{' '}
-            </Header>{' '}
-          </Segment>{' '}
-        </Container>{' '}
+      <div className="portfolio">
+        <div className="container">
+          <NavBar />
+        </div>
       </div>
     );
   }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,34 @@
+import AppBar from '@material-ui/core/AppBar';
+import Avatar from '@material-ui/core/Avatar';
+import Toolbar from '@material-ui/core/Toolbar';
+import React from 'react';
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="navbar">
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <div className="navbar-content">
+              <div className="navbar-content-left">
+                <Avatar
+                  alt="Lalit Kumar"
+                  src="https://avatars1.githubusercontent.com/u/10762218?s=460&v=4"
+                  className="navbar-avatar"
+                />
+                <div className="navbar-brand">Lalit Kumar</div>
+              </div>
+              <div className="navbar-content-right">Hello</div>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
