e4a2490aa14e5e9e6778a81b983cb79134db5f9d
lalitmee <lalitkumar.meena.lk@gmail.com>
2019-01-07 23:51:12 +0530

    HomePage added and background image is on HomePage

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,30 +4,28 @@ import Toolbar from '@material-ui/core/Toolbar';
 import React from 'react';
 import RightDrawer from './RightDrawer';
 
-class NavBar extends React.Component {
-  render() {
-    return (
-      <div className="navbar">
-        <AppBar className="appbar" position="static" color="default">
-          <Toolbar>
-            <div className="navbar-content">
-              <div className="navbar-content-left">
-                <Avatar
-                  alt="Lalit Kumar"
-                  src="https://avatars1.githubusercontent.com/u/10762218?s=460&v=4"
-                  className="navbar-avatar"
-                />
-                <div className="navbar-brand"> Lalit Kumar </div>{' '}
-              </div>{' '}
-              <div className="navbar-content-right">
-                <RightDrawer />
-              </div>{' '}
-            </div>{' '}
-          </Toolbar>{' '}
-        </AppBar>{' '}
-      </div>
-    );
-  }
+function NavBar() {
+  return (
+    <div className="navbar">
+      <AppBar className="appbar" position="static" color="default">
+        <Toolbar>
+          <div className="navbar-content">
+            <div className="navbar-content-left">
+              <Avatar
+                alt="Lalit Kumar"
+                src="https://avatars1.githubusercontent.com/u/10762218?s=460&v=4"
+                className="navbar-avatar"
+              />
+              <div className="navbar-brand">Lalit Kumar</div>
+            </div>
+            <div className="navbar-content-right">
+              <RightDrawer />
+            </div>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
 export default NavBar;
