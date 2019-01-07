64edb94226b0583ab2b7eff278952cbb3e4afad3
lalitmee <lalitkumar.meena.lk@gmail.com>
2019-01-08 00:53:23 +0530

    Name added as heading and typed.js react library installed

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,7 +1,6 @@
 import Paper from '@material-ui/core/Paper';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import PropTypes from 'prop-types';
 import React from 'react';
 
 const styles = theme => ({
@@ -12,25 +11,29 @@ const styles = theme => ({
   }
 });
 
-function HomePage(props) {
-  const { classes } = props;
+function HomePage() {
   return (
     <div className="main-wrapper">
       <Paper className="main-wrapper-paper" elevation={1}>
-        <Typography variant="h5" component="h3">
-          This is a sheet of paper.
-        </Typography>
-        <Typography component="p">
-          Paper can be used to build surface or other elements for your
-          application.
-        </Typography>
+        <div className="name-head ">
+          <Typography
+            className="main-wrapper-first-head"
+            variant="h2"
+            component="div"
+          >
+            Lalit
+          </Typography>
+          <Typography
+            variant="h1"
+            component="div"
+            className="main-wrapper-second-head"
+          >
+            Kumar
+          </Typography>
+        </div>
       </Paper>
     </div>
   );
 }
-
-HomePage.propTypes = {
-  classes: PropTypes.object.isRequired
-};
 
 export default withStyles(styles)(HomePage);
