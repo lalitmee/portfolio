d9b5ed8b0685878394e1b2563c418e5fafb7f6a0
lalitmee <lalitkumar.meena.lk@gmail.com>
2019-01-10 00:54:14 +0530

    animations on scroll added

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -3,6 +3,7 @@ import Paper from '@material-ui/core/Paper';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import React from 'react';
+import OnScroll from 'react-on-scroll';
 import Typist from 'react-typist';
 import TypistLoop from 'react-typist-loop';
 import 'react-typist/dist/Typist.css';
@@ -22,142 +23,208 @@ const styles = theme => ({
   }
 });
 
-function HomePage() {
-  return (
-    <div className="main-wrapper">
-      <Paper className="main-wrapper-paper" elevation={1}>
-        <NavBar />
-        <div className="avatar-image">
-          <Avatar
-            alt="Lalit Kumar"
-            src="https://avatars1.githubusercontent.com/u/10762218?s=460&v=4"
-            className="main-avatar"
-          />
-        </div>
-        <div className="name-head ">
-          <Typography
-            className="main-wrapper-first-head"
-            variant="h1"
-            component="div"
-          >
-            Lalit
-          </Typography>
-          <Typography
-            variant="h1"
-            component="div"
-            className="main-wrapper-second-head"
-          >
-            Kumar
-          </Typography>
-        </div>
-        <div className="typed-text">
-          <TypistLoop interval={500}>
-            {[
-              'Software Developer',
-              'Front End Developer',
-              'Open Source Enthusiast',
-              'Footballer',
-              'Music Lover'
-            ].map(text => (
-              <Typist key={text} startDelay={100}>
-                {text}
-                <Typist.Delay ms={500} />
-                <Typist.Backspace count={text.length} />
-              </Typist>
-            ))}
-          </TypistLoop>
-        </div>
-        <div className="social-icons-wrapper">
-          <ul className="social-icons">
-            <li>
-              <a
-                href="https://www.linkedin.com/in/lalitmee/"
-                target="_blank"
-                className="social-square"
-                rel="noopener noreferrer"
-              >
-                <img
-                  src={linkedIn}
-                  alt="linkedin-icon"
-                  className="social-icon"
-                />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.github.com/lalitmee/"
-                target="_blank"
-                className="social-square"
-                rel="noopener noreferrer"
-              >
-                <img src={github} alt="facebook-icon" className="social-icon" />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://stackoverflow.com/users/4515657/lalit-kumar?tab=profile"
-                target="_blank"
-                className="social-square"
-                rel="noopener noreferrer"
-              >
-                <img
-                  src={stackoverflow}
-                  alt="stackeoverflow-icon"
-                  className="social-icon"
-                />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.facebook.com/iamlalitmee"
-                target="_blank"
-                className="social-square"
-                rel="noopener noreferrer"
-              >
-                <img
-                  src={facebook}
-                  alt="facebook-icon"
-                  className="social-icon"
-                />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.twitter.com/lalitmee/"
-                target="_blank"
-                className="social-square"
-                rel="noopener noreferrer"
-              >
-                <img src={twitter} alt="twitter-icon" className="social-icon" />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.instagram.com/lalitmee/"
-                target="_blank"
-                className="social-square"
-                rel="noopener noreferrer"
-              >
-                <img
-                  src={instagram}
-                  alt="instagram-icon"
-                  className="social-icon"
-                />
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="quote-wrapper">
-          <Typography className="quote-text" variant="h5" component="div">
-            Don't comment bad code - rewrite it.
-          </Typography>
-          <Typography className="quote-writer" variant="h5" component="div">
-            - Brian Kernighan
-          </Typography>
-        </div>
-      </Paper>
-    </div>
-  );
-}
+class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      avatarDiv: false,
+      nameDiv: false,
+      mainWrapperDiv: false
+    };
+  }
 
+  avatarHandle = avatarDiv => {
+    this.setState({ avatarDiv });
+  };
+
+  nameHandle = nameDiv => {
+    this.setState({ nameDiv });
+  };
+
+  mainWrapperHandle = mainWrapperDiv => {
+    this.setState({ mainWrapperDiv });
+  };
+
+  render() {
+    const { nameDiv, avatarDiv, mainWrapperDiv } = this.state;
+    return (
+      <div className="main-wrapper">
+        <OnScroll
+          className="main-wrapper-paper"
+          triggers={[
+            {
+              top: -780,
+              callback: visible => this.mainWrapperHandle(visible)
+            }
+          ]}
+        >
+          <Paper className="main-wrapper-paper" elevation={1}>
+            <NavBar
+              fullName={nameDiv}
+              avatar={avatarDiv}
+              wrapperDiv={mainWrapperDiv}
+            />
+            <div className="avatar-image">
+              <OnScroll
+                className="main-avatar"
+                triggers={[
+                  {
+                    top: -140,
+                    callback: visible => this.avatarHandle(visible)
+                  }
+                ]}
+              >
+                <Avatar
+                  alt="Lalit Kumar"
+                  src="https://avatars1.githubusercontent.com/u/10762218?s=460&v=4"
+                  className="main-avatar"
+                />
+              </OnScroll>
+            </div>
+
+            <OnScroll
+              className="name-head"
+              triggers={[
+                {
+                  top: 0,
+                  callback: visible => this.nameHandle(visible)
+                }
+              ]}
+            >
+              <div className="name-head ">
+                <Typography
+                  className="main-wrapper-first-head"
+                  variant="h1"
+                  component="div"
+                >
+                  Lalit
+                </Typography>
+                <Typography
+                  variant="h1"
+                  component="div"
+                  className="main-wrapper-second-head"
+                >
+                  Kumar
+                </Typography>
+              </div>
+            </OnScroll>
+            <div className="typed-text">
+              <TypistLoop interval={500}>
+                {[
+                  'Software Developer',
+                  'Front End Developer',
+                  'Open Source Enthusiast',
+                  'Footballer',
+                  'Music Lover'
+                ].map(text => (
+                  <Typist key={text} startDelay={100}>
+                    {text}
+                    <Typist.Delay ms={500} />
+                    <Typist.Backspace count={text.length} />
+                  </Typist>
+                ))}
+              </TypistLoop>
+            </div>
+            <div className="social-icons-wrapper">
+              <ul className="social-icons">
+                <li>
+                  <a
+                    href="https://www.linkedin.com/in/lalitmee/"
+                    target="_blank"
+                    className="social-square"
+                    rel="noopener noreferrer"
+                  >
+                    <img
+                      src={linkedIn}
+                      alt="linkedin-icon"
+                      className="social-icon"
+                    />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.github.com/lalitmee/"
+                    target="_blank"
+                    className="social-square"
+                    rel="noopener noreferrer"
+                  >
+                    <img
+                      src={github}
+                      alt="facebook-icon"
+                      className="social-icon"
+                    />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://stackoverflow.com/users/4515657/lalit-kumar?tab=profile"
+                    target="_blank"
+                    className="social-square"
+                    rel="noopener noreferrer"
+                  >
+                    <img
+                      src={stackoverflow}
+                      alt="stackeoverflow-icon"
+                      className="social-icon"
+                    />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.facebook.com/iamlalitmee"
+                    target="_blank"
+                    className="social-square"
+                    rel="noopener noreferrer"
+                  >
+                    <img
+                      src={facebook}
+                      alt="facebook-icon"
+                      className="social-icon"
+                    />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.twitter.com/lalitmee/"
+                    target="_blank"
+                    className="social-square"
+                    rel="noopener noreferrer"
+                  >
+                    <img
+                      src={twitter}
+                      alt="twitter-icon"
+                      className="social-icon"
+                    />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.instagram.com/lalitmee/"
+                    target="_blank"
+                    className="social-square"
+                    rel="noopener noreferrer"
+                  >
+                    <img
+                      src={instagram}
+                      alt="instagram-icon"
+                      className="social-icon"
+                    />
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="quote-wrapper">
+              <Typography className="quote-text" variant="h5" component="div">
+                Don't comment bad code - rewrite it.
+              </Typography>
+              <Typography className="quote-writer" variant="h5" component="div">
+                - Brian Kernighan
+              </Typography>
+            </div>
+          </Paper>
+        </OnScroll>
+      </div>
+    );
+  }
+}
 export default withStyles(styles)(HomePage);
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,8 @@
 import AppBar from '@material-ui/core/AppBar';
+import Avatar from '@material-ui/core/Avatar';
 import { createMuiTheme } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
+import PropTypes from 'prop-types';
 import React from 'react';
 import RightDrawer from './RightDrawer';
 
@@ -11,28 +13,58 @@ const theme = createMuiTheme({
   }
 });
 
-function NavBar() {
-  return (
-    <div className="navbar">
-      <AppBar theme={theme} className="appbar" position="fixed" color="default">
-        <Toolbar>
-          <div className="navbar-content">
-            <div className="navbar-content-left">
-              {/* <Avatar
-                alt="Lalit Kumar"
-                src="https://avatars1.githubusercontent.com/u/10762218?s=460&v=4"
-                className="navbar-avatar"
-              /> */}
-              <div className="navbar-brand">Lalit</div>
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { avatar, fullName, wrapperDiv } = this.props;
+    return (
+      <div className="navbar">
+        <AppBar
+          theme={theme}
+          className={!wrapperDiv ? 'appbar-background-img' : 'appbar'}
+          position="fixed"
+        >
+          <Toolbar>
+            <div className="navbar-content">
+              <div className="navbar-content-left">
+                {!avatar ? (
+                  <Avatar
+                    alt="Lalit Kumar"
+                    src="https://avatars1.githubusercontent.com/u/10762218?s=460&v=4"
+                    className="navbar-avatar"
+                  />
+                ) : (
+                  ''
+                )}
+                {!fullName ? (
+                  <div
+                    className={avatar ? 'navbar-brand' : 'navbar-brand-animate'}
+                  >
+                    Lalit Kumar
+                  </div>
+                ) : (
+                  <div className="navbar-brand">Lalit</div>
+                )}
+              </div>
+              <div className="navbar-content-right">
+                <RightDrawer />
+              </div>
             </div>
-            <div className="navbar-content-right">
-              <RightDrawer />
-            </div>
-          </div>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
 }
+
+NavBar.propTypes = {
+  avatar: PropTypes.bool.isRequired,
+  fullName: PropTypes.bool.isRequired,
+  wrapperDiv: PropTypes.bool.isRequired
+};
 
 export default NavBar;
