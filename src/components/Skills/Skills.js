1139cd84befc3edc7d3344a685775e238d86fc45
lalitmee <lalitkumar.meena.lk@gmail.com>
2019-01-12 02:15:25 +0530

    skills section started

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -33,15 +33,38 @@ class Skills extends React.Component {
     };
   }
 
-  componentWillMount() {
-    this.setState({
-      angular: 70,
-      react: 60,
-      html: 100,
-      css: 100,
-      javascript: 90,
-      jquery: 90
-    });
+  componentDidMount() {
+    const nodes = [].slice.call(document.getElementsByClassName('card'), 0);
+    const directions = { 0: 'top', 1: 'right', 2: 'bottom', 3: 'left' };
+    const classNames = ['in', 'out']
+      .map(p => Object.values(directions).map(d => `${p}-${d}`))
+      .reduce((a, b) => a.concat(b));
+
+    const getDirectionKey = (ev, node) => {
+      const { width, height, top, left } = node.getBoundingClientRect();
+      const l = ev.pageX - (left + window.pageXOffset);
+      const t = ev.pageY - (top + window.pageYOffset);
+      const x = l - (width / 2) * (width > height ? height / width : 1);
+      const y = t - (height / 2) * (height > width ? width / height : 1);
+      return Math.round(Math.atan2(y, x) / 1.57079633 + 5) % 4;
+    };
+
+    class Item {
+      constructor(element) {
+        this.element = element;
+        this.element.addEventListener('mouseover', ev => this.update(ev, 'in'));
+        this.element.addEventListener('mouseout', ev => this.update(ev, 'out'));
+      }
+
+      update(ev, prefix) {
+        this.element.classList.remove(...classNames);
+        this.element.classList.add(
+          `${prefix}-${directions[getDirectionKey(ev, this.element)]}`
+        );
+      }
+    }
+
+    nodes.forEach(node => new Item(node));
   }
 
   render() {
@@ -62,6 +85,13 @@ class Skills extends React.Component {
                     </Typography>
                   </CardContent>
                 </div>
+                <div className="info">
+                  <h3>Single-origin coffee whatever</h3>
+                  <p>
+                    Williamsburg tofu polaroid, 90s Bushwick irony locavore
+                    ethnic meh messenger bag Truffaut jean shorts.
+                  </p>
+                </div>
               </Card>
             </Grid>
             <Grid className="grid-item" item xs={6}>
@@ -73,6 +103,13 @@ class Skills extends React.Component {
                       Research & Analytics
                     </Typography>
                   </CardContent>
+                </div>
+                <div className="info">
+                  <h3>Single-origin coffee whatever</h3>
+                  <p>
+                    Williamsburg tofu polaroid, 90s Bushwick irony locavore
+                    ethnic meh messenger bag Truffaut jean shorts.
+                  </p>
                 </div>
               </Card>
             </Grid>
@@ -90,6 +127,13 @@ class Skills extends React.Component {
                     </Typography>
                   </CardContent>
                 </div>
+                <div className="info">
+                  <h3>Single-origin coffee whatever</h3>
+                  <p>
+                    Williamsburg tofu polaroid, 90s Bushwick irony locavore
+                    ethnic meh messenger bag Truffaut jean shorts.
+                  </p>
+                </div>
               </Card>
             </Grid>
             <Grid className="grid-item" item xs={6}>
@@ -101,6 +145,13 @@ class Skills extends React.Component {
                       Team Work
                     </Typography>
                   </CardContent>
+                </div>
+                <div className="info">
+                  <h3>Single-origin coffee whatever</h3>
+                  <p>
+                    Williamsburg tofu polaroid, 90s Bushwick irony locavore
+                    ethnic meh messenger bag Truffaut jean shorts.
+                  </p>
                 </div>
               </Card>
             </Grid>
