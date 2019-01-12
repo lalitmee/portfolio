d6a409b26b5e1d40433fddd8aaf1c8cc31b21cab
lalitmee <lalitkumar.meena.lk@gmail.com>
2019-01-13 00:21:41 +0530

    Project section and navigation smooth scroll added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Element } from 'react-scroll';
 import './App.css';
 import HomePage from './components/HomePage/HomePage';
 import Skills from './components/Skills/Skills';
+import Projects from './components/Projects/Projects';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -12,8 +14,15 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="portfolio">
-        <HomePage />
-        <Skills />
+        <Element name="home" className="element">
+          <HomePage />
+        </Element>
+        <Element name="skills" className="element">
+          <Skills />
+        </Element>
+        <Element name="projects" className="element">
+          <Projects />
+        </Element>
         {/* <div className="container">
         </div> */}
       </div>
