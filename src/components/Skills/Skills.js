f8bc50c64b1ac8caf7d991e2126f7405187def8c
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2019-01-19 00:25:30 +0530

    font changed to Proxima

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -37,7 +37,7 @@ class Skills extends React.Component {
       <div className="skills-wrapper" id="skills">
         <Paper className="skills-wrapper-paper" elevation={1}>
           <Typography className="skills-head" variant="h3" component="div">
-            Look What I can do
+            Skills
           </Typography>
           <hr className="horizontal-rule" />
           <Grid
