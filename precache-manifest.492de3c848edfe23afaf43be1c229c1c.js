45a0c0ef896c8523fa1fcf9d629153e1b58dcd4d
lalitmee <lalitkumar.meena.lk@gmail.com>
2019-08-11 23:01:08 +0530

    Updates

---
+++ precache-manifest.492de3c848edfe23afaf43be1c229c1c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3da6ff7b32e55519968c081cdb5be76",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9d139286c46dc7702435",
+    "url": "/portfolio/static/css/2.f91b8490.chunk.css"
+  },
+  {
+    "revision": "4cc4ccfcdadee78bbb00",
+    "url": "/portfolio/static/css/main.d15f4128.chunk.css"
+  },
+  {
+    "revision": "9d139286c46dc7702435",
+    "url": "/portfolio/static/js/2.4406d71d.chunk.js"
+  },
+  {
+    "revision": "4cc4ccfcdadee78bbb00",
+    "url": "/portfolio/static/js/main.c97ead5e.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "c5241d5456b68a4418c6b17ce287d9cf",
+    "url": "/portfolio/static/media/back5.c5241d54.png"
+  },
+  {
+    "revision": "0ab5a87e8ae0b324879ac2b89c2c4bcd",
+    "url": "/portfolio/static/media/image-loader.0ab5a87e.gif"
+  },
+  {
+    "revision": "f1448054f14757d284c064b271c00cc6",
+    "url": "/portfolio/static/media/proxima.f1448054.ttf"
+  },
+  {
+    "revision": "c0e7f075f9c77cfff95e401b449ec29a",
+    "url": "/portfolio/static/media/resume-image.c0e7f075.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/portfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
